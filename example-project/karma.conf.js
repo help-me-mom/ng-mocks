31858e6e7023bfe6abcfc661087334ab283c5d68
erikbarke <e@barke.nu>
2016-09-03 05:58:02 +0200

    Merged examples into one

---
+++ example-project/karma.conf.js
@@ -0,0 +1,18 @@
+module.exports = function(config) {
+    config.set({
+
+        frameworks: ["jasmine", "karma-typescript"],
+
+        files: [
+            { pattern: "src/**/*.ts" }
+        ],
+
+        preprocessors: {
+            "**/*.ts": ["karma-typescript"]
+        },
+
+        reporters: ["progress", "karma-typescript"],
+
+        browsers: ["Chrome"]
+    });
+};
