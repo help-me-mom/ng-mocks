557428645bd35c0f551bbf6875e7c2129e639e4e
erikbarke <e@barke.nu>
2016-09-03 05:59:19 +0200

    Added linting

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "node": true
+    },
+    "globals": {
+        "Promise": true,
+        "WeakMap": true
+    },
+    "rules": {
+
+        // syntax
+        "curly": ["error"],
+        "dot-notation": ["error"],
+        "eqeqeq": ["error"],
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "no-alert": ["error"],
+        "no-cond-assign": ["error", "always"],
+        "no-empty-function": ["error"],
+        "no-unused-expressions": ["error"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"],
+
+        // style
+        "brace-style": ["error", "stroustrup"]
+    }
+}
