217c3a020e399b6dbf70ef34e17220a36a21ad34
erikbarke <e@barke.nu>
2016-11-23 17:38:26 +0100

    Example project for Mocha, #37

---
+++ example-project@mocha/karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+    config.set({
+
+        frameworks: ["mocha", "karma-typescript"],
+
+        files: [
+            { pattern: "node_modules/expect.js/index.js" },
+            { pattern: "src/**/*.ts" }
+        ],
+
+        preprocessors: {
+            "**/*.ts": ["karma-typescript"]
+        },
+
+        reporters: ["progress", "karma-typescript"],
+
+        browsers: ["Chrome"]
+    });
+};
