64787d5cf0bcec5edd7d1dd5d3f6fe611018eb61
erikbarke <e@barke.nu>
2016-12-10 06:40:59 +0100

    Added missing css file and compiler options

--- example-project@angular2/src/app/hello.ts
+++ example-project@angular2/src/app/hello.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-hello",
     templateUrl: "app/hello.html",
-    styles: [require("../../assets/style/main.css")]
+    styles: [require("../assets/style/main.css")]
 })
 
 export class HelloComponent {
