2f33e7d3a49821a4a187a087302aef1789c40f0a
erikbarke <e@barke.nu>
2017-02-13 06:11:36 +0100

    Moved client side javascript to src folder

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,9 @@ gulp.task("copy", function(cb) {
     copy("../../*.js", "./node_modules/karma-typescript/", function() {
         copy("../../dist/*.js", "./node_modules/karma-typescript/dist/", function() {
             copy("../../lib/*.js", "./node_modules/karma-typescript/lib/", function() {
-                copy("../../transforms/*.js", "./node_modules/karma-typescript/transforms/", cb);
+                copy("../../src/client/*.js", "./node_modules/karma-typescript/src/client/", function() {
+                    copy("../../transforms/*.js", "./node_modules/karma-typescript/transforms/", cb);
+                });
             });
         });
     });
