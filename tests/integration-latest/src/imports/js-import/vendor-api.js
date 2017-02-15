959cd861fcaeca70c37b5f36580b0ebebbbfc167
erikbarke <e@barke.nu>
2017-02-15 21:01:26 +0100

    No support for ES6 yet, #90

--- tests/integration-latest/src/imports/js-import/vendor-api.js
+++ tests/integration-latest/src/imports/js-import/vendor-api.js
@@ -1,9 +1,27 @@
-const urls = require('./urls');
+// ES5 version
+var urls = require("./urls");
+
+module.exports = {
+    list: function(authToken, parameters){
+        return {
+            method: "POST",
+            url: urls.vendor.create,
+            headers: {
+                authorization: "token " + authToken
+            },
+            data: parameters
+        };
+    }
+};
+
+/*
+// ES6 version
+const urls = require("./urls");
 
 module.exports = {
     list(authToken, parameters){
         return {
-            method: 'POST',
+            method: "POST",
             url: urls.vendor.create,
             headers: {
                 authorization: `token ${authToken}`
@@ -12,3 +30,5 @@ module.exports = {
         };
     }
 };
+
+*/
