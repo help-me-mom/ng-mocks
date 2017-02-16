7ba15c7d0d7aeb4aa7744d4309980ea5aaf57bb3
erikbarke <e@barke.nu>
2017-02-16 23:40:41 +0100

    Corrected file paths

--- ci/obliterate.sh
+++ ci/obliterate.sh
@@ -8,5 +8,5 @@ rm -rf examples/gulp/node_modules/ examples/gulp/src/bar/coverage/ examples/gulp
 rm -rf examples/mocha/coverage/ examples/mocha/node_modules/
 rm -rf examples/typescript-1.6.2/coverage/ examples/typescript-1.6.2/node_modules/
 rm -rf examples/typescript-latest/coverage/ examples/typescript-latest/node_modules/
-rm -rf tests/integration/1.8.10/coverage/ tests/integration/1.8.10/node_modules/
-rm -rf tests/integration/latest/coverage/ tests/integration/latest/node_modules/
+rm -rf tests/integration-1.8.10/coverage/ tests/integration-1.8.10/node_modules/
+rm -rf tests/integration-latest/coverage/ tests/integration-latest/node_modules/
