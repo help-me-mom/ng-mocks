7abddd8d38ea6b05a6e40291f66383c5f88d6074
erikbarke <e@barke.nu>
2017-02-23 22:35:20 +0100

    Angular2 example with css, less and scss, #69

--- examples/angular2/src/app/hello.ts
+++ examples/angular2/src/app/hello.ts
@@ -1,16 +1,12 @@
 import { Component } from "@angular/core";
 
 @Component({
-
     // use moduleId with proxies: { "/src/app/": "/base/src/app/" } in karma.conf.js
     // moduleId: module.id,
     selector: "app-hello",
-
-    styleUrls: ["../assets/style/main.css"],
-
-    // use templateUrl without moduleId + karma-typescript/transforms/angular2-template-url-rewriter in karma.conf.js
+    styleUrls: ["../assets/style/main.css", "../assets/style/main.less", "../assets/style/main.scss"],
+    // use templateUrl without moduleId + karma-typescript/transforms/angular2-transform in karma.conf.js
     templateUrl: "hello.html"
-
     // use templateUrl without moduleId + proxies: {"/app/": "/base/src/app"} in karma.conf.js
     // templateUrl: "app/hello.html",
 })
