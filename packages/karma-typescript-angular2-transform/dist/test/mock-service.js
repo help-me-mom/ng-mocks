dc5dfc763455041b1587291a4e6cb285a5bd11bc
erikbarke <e@barke.nu>
2017-04-14 19:51:49 +0200

    Inline source maps

--- packages/karma-typescript-angular2-transform/dist/test/mock-service.js
+++ packages/karma-typescript-angular2-transform/dist/test/mock-service.js
@@ -6,4 +6,4 @@ var MockService = (function () {
     return MockService;
 }());
 exports.MockService = MockService;
-//# sourceMappingURL=mock-service.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9jay1zZXJ2aWNlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL3Rlc3QvbW9jay1zZXJ2aWNlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUE7SUFBQTtJQUEwQixDQUFDO0lBQUQsa0JBQUM7QUFBRCxDQUFDLEFBQTNCLElBQTJCO0FBQWQsa0NBQVcifQ==
