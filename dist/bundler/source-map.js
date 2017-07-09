dcc8f4c5daffbd01b84a6c5e84434d71c94947ef
erikbarke <e@barke.nu>
2017-07-09 23:19:59 +0200

    Setting source map property for Karma/reporter.js, #142

--- dist/bundler/source-map.js
+++ dist/bundler/source-map.js
@@ -10,6 +10,7 @@ function create(file, source, emitOutput) {
         map.sources[0] = path.basename(file.originalPath);
         map.sourcesContent = [source];
         map.file = path.basename(file.path);
+        file.sourceMap = map;
         datauri = "data:application/json;charset=utf-8;base64," + new Buffer(JSON.stringify(map)).toString("base64");
         result = result.replace(createComment(file), "//# sourceMappingURL=" + datauri);
     }
--- src/shared/file.ts
+++ src/shared/file.ts
@@ -1,4 +1,5 @@
 export interface File {
     originalPath: string;
     path: string;
+    sourceMap: string;
 };
