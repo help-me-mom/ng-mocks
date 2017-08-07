97c05a1dec9c428a441a0eecc099a35f2e156ada
erikbarke <e@barke.nu>
2017-08-07 20:29:35 +0200

    Make the compiler happy

--- packages/karma-typescript-angular2-transform/dist/test/another-mock-component.js
+++ packages/karma-typescript-angular2-transform/dist/test/another-mock-component.js
@@ -10,14 +10,14 @@ var core_1 = require("@angular/core");
 var AnotherMockComponent = (function () {
     function AnotherMockComponent() {
     }
-    AnotherMockComponent = __decorate([
-        core_1.Component({
-            selector: "app-mock",
-            styleUrls: ["style.css", "./style.less", "../style.scss"],
-            templateUrl: "mock.html"
-        })
-    ], AnotherMockComponent);
     return AnotherMockComponent;
 }());
+AnotherMockComponent = __decorate([
+    core_1.Component({
+        selector: "app-mock",
+        styleUrls: ["style.css", "./style.less", "../style.scss"],
+        templateUrl: "mock.html"
+    })
+], AnotherMockComponent);
 exports.AnotherMockComponent = AnotherMockComponent;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5vdGhlci1tb2NrLWNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy90ZXN0L2Fub3RoZXItbW9jay1jb21wb25lbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQSxzQ0FBMEM7QUFPMUM7SUFBQTtJQUFtQyxDQUFDO0lBQXZCLG9CQUFvQjtRQUxoQyxnQkFBUyxDQUFDO1lBQ1AsUUFBUSxFQUFFLFVBQVU7WUFDcEIsU0FBUyxFQUFFLENBQUMsV0FBVyxFQUFFLGNBQWMsRUFBRSxlQUFlLENBQUM7WUFDekQsV0FBVyxFQUFFLFdBQVc7U0FDM0IsQ0FBQztPQUNXLG9CQUFvQixDQUFHO0lBQUQsMkJBQUM7Q0FBQSxBQUFwQyxJQUFvQztBQUF2QixvREFBb0IifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5vdGhlci1tb2NrLWNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy90ZXN0L2Fub3RoZXItbW9jay1jb21wb25lbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQSxzQ0FBMEM7QUFPMUMsSUFBYSxvQkFBb0I7SUFBakM7SUFBbUMsQ0FBQztJQUFELDJCQUFDO0FBQUQsQ0FBQyxBQUFwQyxJQUFvQztBQUF2QixvQkFBb0I7SUFMaEMsZ0JBQVMsQ0FBQztRQUNQLFFBQVEsRUFBRSxVQUFVO1FBQ3BCLFNBQVMsRUFBRSxDQUFDLFdBQVcsRUFBRSxjQUFjLEVBQUUsZUFBZSxDQUFDO1FBQ3pELFdBQVcsRUFBRSxXQUFXO0tBQzNCLENBQUM7R0FDVyxvQkFBb0IsQ0FBRztBQUF2QixvREFBb0IifQ==
--- packages/karma-typescript-angular2-transform/dist/test/mock-component.js
+++ packages/karma-typescript-angular2-transform/dist/test/mock-component.js
@@ -10,14 +10,14 @@ var core_1 = require("@angular/core");
 var MockComponent = (function () {
     function MockComponent() {
     }
-    MockComponent = __decorate([
-        core_1.Component({
-            selector: "app-mock",
-            styleUrls: ["style.css", "./style.less", "../style.scss"],
-            templateUrl: "mock.html"
-        })
-    ], MockComponent);
     return MockComponent;
 }());
+MockComponent = __decorate([
+    core_1.Component({
+        selector: "app-mock",
+        styleUrls: ["style.css", "./style.less", "../style.scss"],
+        templateUrl: "mock.html"
+    })
+], MockComponent);
 exports.MockComponent = MockComponent;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9jay1jb21wb25lbnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvdGVzdC9tb2NrLWNvbXBvbmVudC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBLHNDQUEwQztBQU8xQztJQUFBO0lBQTRCLENBQUM7SUFBaEIsYUFBYTtRQUx6QixnQkFBUyxDQUFDO1lBQ1AsUUFBUSxFQUFFLFVBQVU7WUFDcEIsU0FBUyxFQUFFLENBQUMsV0FBVyxFQUFFLGNBQWMsRUFBRSxlQUFlLENBQUM7WUFDekQsV0FBVyxFQUFFLFdBQVc7U0FDM0IsQ0FBQztPQUNXLGFBQWEsQ0FBRztJQUFELG9CQUFDO0NBQUEsQUFBN0IsSUFBNkI7QUFBaEIsc0NBQWEifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9jay1jb21wb25lbnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvdGVzdC9tb2NrLWNvbXBvbmVudC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBLHNDQUEwQztBQU8xQyxJQUFhLGFBQWE7SUFBMUI7SUFBNEIsQ0FBQztJQUFELG9CQUFDO0FBQUQsQ0FBQyxBQUE3QixJQUE2QjtBQUFoQixhQUFhO0lBTHpCLGdCQUFTLENBQUM7UUFDUCxRQUFRLEVBQUUsVUFBVTtRQUNwQixTQUFTLEVBQUUsQ0FBQyxXQUFXLEVBQUUsY0FBYyxFQUFFLGVBQWUsQ0FBQztRQUN6RCxXQUFXLEVBQUUsV0FBVztLQUMzQixDQUFDO0dBQ1csYUFBYSxDQUFHO0FBQWhCLHNDQUFhIn0=
