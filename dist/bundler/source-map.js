68412ee87f1221d24e2384e019dbd874e79c39a2
erikbarke <e@barke.nu>
2017-08-13 00:18:39 +0200

    Cleanup

--- dist/bundler/source-map.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var path = require("path");
-function createComment(file) {
-    return "//# sourceMappingURL=" + path.basename(file.path) + ".map";
-}
-exports.createComment = createComment;
