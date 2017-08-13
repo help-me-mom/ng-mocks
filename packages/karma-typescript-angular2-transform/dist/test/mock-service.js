791a5a9748df6d21d5b0cf89458af95211ac48cc
erikbarke <e@barke.nu>
2017-08-13 13:59:38 +0200

    Moving from old Istanbul cli to nyc, monounity/karma-typescript#158

--- packages/karma-typescript-angular2-transform/dist/test/mock-service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MockService = (function () {
-    function MockService() {
-    }
-    return MockService;
-}());
-exports.MockService = MockService;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9jay1zZXJ2aWNlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL3Rlc3QvbW9jay1zZXJ2aWNlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUE7SUFBQTtJQUEwQixDQUFDO0lBQUQsa0JBQUM7QUFBRCxDQUFDLEFBQTNCLElBQTJCO0FBQWQsa0NBQVcifQ==
