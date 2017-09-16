76f5b1235ce9389cbeb44dec59e478fb7497b56f
Rohit Sinha <rohit@therrsgroup.com>
2017-09-16 19:16:44 -0400

    WIP - Routing to ng2-mock-component

--- lib/mock_component.spec.ts
+++ lib/mock_component.spec.ts
@@ -1,19 +1,43 @@
-import 'reflect-metadata';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MockComponent } from './mock_component';
+import { TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 @Component({
-  selector: 'some-selector',
-  template: ''
+  selector: 'parent',
+  template: '<child [a]="a"></child>'
 })
-class ExampleComponent {
-  @Input() someInput: string;
-  @Output() someOutput: EventEmitter<string>;
+export class ParentComponent {
+  a = 'Unit Testing';
 }
 
-describe('MockComponent', () => {
-  it('should mock the component', () => {
-    MockComponent(ExampleComponent);
-    expect(false).toBeTruthy();
+@Component({
+  selector: 'child',
+  template: '{{a}}'
+})
+export class ChildComponent {
+  @Input()
+  a: String;
+}
+
+describe('ParentComponent', () => {
+  let fixture: any,
+    element: any;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        ParentComponent,
+        MockComponent(ChildComponent)
+      ]
+    });
+
+    fixture = TestBed.createComponent(ParentComponent);
+    element = fixture.debugElement;
+  }));
+
+  it('should mock the child component', () => {
+    fixture.detectChanges();
+    expect(element.query(By.css('child'))).not.toBeNull();
+    console.log(element);
   });
 });
--- lib/mock_component.ts
+++ lib/mock_component.ts
@@ -1,35 +1,28 @@
 import 'reflect-metadata';
+import { Component } from '@angular/core';
+import { MockComponent as Ng2MockComponent } from 'ng2-mock-component';
 
 export function MockComponent(component: any) {
-  const annotations = Reflect.getMetadata('annotations', component);
-  const propertyMetadata = Reflect.getMetadata('propMetadata', component);
-  const inputs = [],
-    outputs = [];
-  console.log(Reflect.metadata('design:type', component));
-  console.log(Reflect.getMetadataKeys(component));
-  console.log('**************');
+  const annotations = Reflect.getMetadata('annotations', component),
+    propertyMetadata = Reflect.getMetadata('propMetadata', component),
+    inputs: string[] = [],
+    outputs: string[] = [];
 
-  // const decorator = annotations.find((annotation) => annotation instanceof DecoratorFactory);
-  console.log(annotations[0].selector);
+  let options: Component = {
+    selector: annotations[0].selector,
+    template: 'MOCK',
+    inputs,
+    outputs
+  };
 
   for (var property in propertyMetadata) {
     const prop = propertyMetadata[property];
-    console.log(property);
-    console.log(Reflect.getMetadata('annotations', component));
-    console.log(Reflect.getOwnMetadata('annotations', component, property));
     if (prop[0].toString() === '@Input') {
       inputs.push(property);
     } else if (prop[0].toString() === '@Output') {
       outputs.push(property);
     }
   }
-  console.log(Reflect.getOwnMetadataKeys(component));
-  console.log(Reflect.getOwnMetadata('annotations', component));
-  console.log(Reflect.getOwnMetadata('propMetadata', component));
 
-  // console.log(Reflect.getMetadata('annotations', component));
-  // console.log(Reflect.getMetadata('propMetadata', component));
-  console.log(Reflect.getMetadata('propMetadata', component)['someInput'][0]);
-  console.log('inputs:', inputs);
-  console.log('outputs:', outputs);
+  return Ng2MockComponent(options);
 }
