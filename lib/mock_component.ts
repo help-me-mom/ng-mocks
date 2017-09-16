c7ee70af5d12784ec75ff461345decd327f0e7ec
Rohit Sinha <rohit@therrsgroup.com>
2017-09-16 13:55:55 -0400

    Adding inputs/outputs

--- lib/mock_component.ts
+++ lib/mock_component.ts
@@ -3,6 +3,8 @@ import 'reflect-metadata';
 export function MockComponent(component: any) {
   const annotations = Reflect.getMetadata('annotations', component);
   const propertyMetadata = Reflect.getMetadata('propMetadata', component);
+  const inputs = [],
+    outputs = [];
   console.log(Reflect.metadata('design:type', component));
   console.log(Reflect.getMetadataKeys(component));
   console.log('**************');
@@ -11,9 +13,15 @@ export function MockComponent(component: any) {
   console.log(annotations[0].selector);
 
   for (var property in propertyMetadata) {
+    const prop = propertyMetadata[property];
     console.log(property);
     console.log(Reflect.getMetadata('annotations', component));
     console.log(Reflect.getOwnMetadata('annotations', component, property));
+    if (prop[0].toString() === '@Input') {
+      inputs.push(property);
+    } else if (prop[0].toString() === '@Output') {
+      outputs.push(property);
+    }
   }
   console.log(Reflect.getOwnMetadataKeys(component));
   console.log(Reflect.getOwnMetadata('annotations', component));
@@ -22,4 +30,6 @@ export function MockComponent(component: any) {
   // console.log(Reflect.getMetadata('annotations', component));
   // console.log(Reflect.getMetadata('propMetadata', component));
   console.log(Reflect.getMetadata('propMetadata', component)['someInput'][0]);
+  console.log('inputs:', inputs);
+  console.log('outputs:', outputs);
 }
