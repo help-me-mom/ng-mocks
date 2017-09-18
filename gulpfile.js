0275208049eede38117184fd5964bbc34e7c2b75
Isaac Datlof <ike18t@gmail.com>
2017-09-18 18:02:15 -0400

    cleanup and update package.json and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
-const jasmine = require('gulp-jasmine');
-const istanbul = require('gulp-istanbul');
 const clean = require('gulp-clean');
 const runSequence = require('run-sequence');
 const merge = require('merge2');
@@ -28,27 +26,6 @@ gulp.task('lint', function() {
   return gulp.src(['lib/**', 'spec/**'])
     .pipe(tslint({ formatter: 'stylish' }))
     .pipe(tslint.report());
-});
-
-gulp.task('pre-test', function () {
-  return gulp.src(['dist/lib/**/*.ts'])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
-});
-
-gulp.task('test:run', ['pre-test'], function() {
-  return gulp.src('dist/lib/**/*.spec.js')
-    .pipe(jasmine())
-    .pipe(istanbul.writeReports({ reporters: ['lcov', 'html'] }))
-    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
-});
-
-gulp.task('watch', ['default'], function() {
-    gulp.watch('src/*.ts', ['default']);
-});
-
-gulp.task('test', [], function(cb) {
-  runSequence('clean', 'build', 'test:run', cb);
 });
 
 gulp.task('default', [], function(cb) {
