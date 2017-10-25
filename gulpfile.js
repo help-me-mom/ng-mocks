6f170da5cc26726e55b12fa975c457b6f07fc5a7
erikbarke <e@barke.nu>
2017-10-25 21:53:17 +0200

    Build the source code before running examples/integration tests, #195

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
 var copy = require("copy"),
     clear = require("cli-clear"),
     del = require("del"),
+    exec = require("child_process").exec,
     gulp = require("gulp"),
-    gutil = require("gulp-util");
+    gutil = require("gulp-util"),
+    runSequence = require("run-sequence");
 
 gulp.task("clear", function(cb) {
     clear();
     cb();
+});
+
+gulp.task("clean", function(cb) {
+    del("./coverage")
+    .then(function(files) {
+        if(files.length > 0) {
+            gutil.log("Deleted %s", files);
+        }
+        cb();
+    });
+});
+
+gulp.task("build", function (cb) {
+    exec("../../node_modules/.bin/tsc --project ../../tsconfig.json --rootDir ../../src", function (error, stdout, stderr) {
+        process.stdout.write(stdout);
+        process.stderr.write(stderr);
+        cb(error);
+    });
 });
 
 gulp.task("copy", function(cb) {
@@ -21,15 +41,8 @@ gulp.task("copy", function(cb) {
     });
 });
 
-gulp.task("clean", function(cb) {
-    del("./coverage")
-    .then(function(files) {
-        if(files.length > 0) {
-            gutil.log("Deleted %s", files);
-        }
-        cb();
-    });
+gulp.task("dev", ["clear", "clean"], function(cb) {
+    runSequence("build", "copy", cb);
 });
 
-gulp.task("dev", ["clear", "clean", "copy"]);
 gulp.task("dev:ci", ["clean", "copy"]);
