d771482b4f9e3db82dddd1a4442a0499e817a9c2
erikbarke <e@barke.nu>
2017-10-26 20:54:39 +0200

    Synchronous callback hell instead of windows install hell

--- ci/run.js
+++ ci/run.js
@@ -1,26 +1,41 @@
 var spawn = require("child_process").spawn;
-var wait = require("wait.for");
 
-wait.launchFiber(run);
-
-function run() {
-    wait.for(execute, "examples/angular2",          "npm run dev:ci:phantomjs");
-    wait.for(execute, "examples/angularjs",         "npm run dev:ci:phantomjs");
-    wait.for(execute, "examples/mocha",             "npm run dev:ci:phantomjs");
-    wait.for(execute, "examples/typescript-1.6.2",  "npm run dev:ci:phantomjs");
-    wait.for(execute, "examples/typescript-latest", "npm run dev:ci:phantomjs");
-
-    wait.for(execute, "tests/integration-1.8.10",   "npm run dev:ci:phantomjs");
-    wait.for(execute, "tests/integration-1.8.10",   "npm run dev:ci:phantomjs:angular2");
-    wait.for(execute, "tests/integration-1.8.10",   "npm run dev:ci:phantomjs:react");
-
-    wait.for(execute, "tests/integration-latest",   "npm run dev:ci:phantomjs");
-    wait.for(execute, "tests/integration-latest",   "npm run dev:ci:phantomjs:angular2");
-    wait.for(execute, "tests/integration-latest",   "npm run dev:ci:phantomjs:core");
-    wait.for(execute, "tests/integration-latest",   "npm run dev:ci:phantomjs:emptyfile");
-    wait.for(execute, "tests/integration-latest",   "npm run dev:ci:phantomjs:no-module");
-    wait.for(execute, "tests/integration-latest",   "npm run dev:ci:phantomjs:react");
-}
+// The pyramid of doom :( If you have a better idea, (which doesn't
+// involve wait.for / node-gyp / fibers) please create a pull request,
+// but bear in mind that this has to run on windows machines as well...
+execute("examples/angular2", "npm run dev:ci:phantomjs", function() {
+    execute("examples/angular2", "npm run dev:ci:phantomjs", function() {
+        execute("examples/angularjs", "npm run dev:ci:phantomjs", function() {
+            execute("examples/mocha", "npm run dev:ci:phantomjs", function() {
+                execute("examples/typescript-1.6.2", "npm run dev:ci:phantomjs", function() {
+                    execute("examples/typescript-latest", "npm run dev:ci:phantomjs", function() {
+                    
+                        execute("tests/integration-1.8.10", "npm run dev:ci:phantomjs", function() {
+                            execute("tests/integration-1.8.10", "npm run dev:ci:phantomjs:angular2", function() {
+                                execute("tests/integration-1.8.10", "npm run dev:ci:phantomjs:react", function() {
+                                
+                                    execute("tests/integration-latest", "npm run dev:ci:phantomjs", function() {
+                                        execute("tests/integration-latest", "npm run dev:ci:phantomjs:angular2", function() {
+                                            execute("tests/integration-latest", "npm run dev:ci:phantomjs:core", function() {
+                                                execute("tests/integration-latest", "npm run dev:ci:phantomjs:emptyfile", function() {
+                                                    execute("tests/integration-latest", "npm run dev:ci:phantomjs:no-module", function() {
+                                                        execute("tests/integration-latest", "npm run dev:ci:phantomjs:react", function() {
+                                                            //
+                                                        });
+                                                    });
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
 
 function execute(dir, cmd, callback) {
     
