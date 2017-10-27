ad81de41deef6fa11a79a5b84bfe7b43c1c0f64d
erikbarke <e@barke.nu>
2017-10-27 06:48:21 +0200

    PhantomJS on *nix, Chrome on windows

--- ci/run.js
+++ ci/run.js
@@ -1,25 +1,28 @@
 var spawn = require("cross-spawn");
+var os = require("os");
+
+var browser = os.platform === "win32" ? "" : ":phantomjs";
 
 // The pyramid of doom :( If you have a better idea, (which doesn't
 // involve wait.for / node-gyp / fibers) please create a pull request,
 // but bear in mind that this has to run on windows machines as well...
-execute("examples/angular2", "npm run dev:ci:phantomjs", function() {
-    execute("examples/angular2", "npm run dev:ci:phantomjs", function() {
-        execute("examples/angularjs", "npm run dev:ci:phantomjs", function() {
-            execute("examples/mocha", "npm run dev:ci:phantomjs", function() {
-                execute("examples/typescript-1.6.2", "npm run dev:ci:phantomjs", function() {
-                    execute("examples/typescript-latest", "npm run dev:ci:phantomjs", function() {
+execute("examples/angular2", "npm run dev:ci" + browser, function() {
+    execute("examples/angular2", "npm run dev:ci" + browser, function() {
+        execute("examples/angularjs", "npm run dev:ci" + browser, function() {
+            execute("examples/mocha", "npm run dev:ci" + browser, function() {
+                execute("examples/typescript-1.6.2", "npm run dev:ci" + browser, function() {
+                    execute("examples/typescript-latest", "npm run dev:ci" + browser, function() {
                     
-                        execute("tests/integration-1.8.10", "npm run dev:ci:phantomjs", function() {
-                            execute("tests/integration-1.8.10", "npm run dev:ci:phantomjs:angular2", function() {
-                                execute("tests/integration-1.8.10", "npm run dev:ci:phantomjs:react", function() {
+                        execute("tests/integration-1.8.10", "npm run dev:ci" + browser, function() {
+                            execute("tests/integration-1.8.10", "npm run dev:ci" + browser + ":angular2", function() {
+                                execute("tests/integration-1.8.10", "npm run dev:ci" + browser + ":react", function() {
                                 
-                                    execute("tests/integration-latest", "npm run dev:ci:phantomjs", function() {
-                                        execute("tests/integration-latest", "npm run dev:ci:phantomjs:angular2", function() {
-                                            execute("tests/integration-latest", "npm run dev:ci:phantomjs:core", function() {
-                                                execute("tests/integration-latest", "npm run dev:ci:phantomjs:emptyfile", function() {
-                                                    execute("tests/integration-latest", "npm run dev:ci:phantomjs:no-module", function() {
-                                                        execute("tests/integration-latest", "npm run dev:ci:phantomjs:react", function() {
+                                    execute("tests/integration-latest", "npm run dev:ci" + browser + "", function() {
+                                        execute("tests/integration-latest", "npm run dev:ci" + browser + ":angular2", function() {
+                                            execute("tests/integration-latest", "npm run dev:ci" + browser + ":core", function() {
+                                                execute("tests/integration-latest", "npm run dev:ci" + browser + ":emptyfile", function() {
+                                                    execute("tests/integration-latest", "npm run dev:ci" + browser + ":no-module", function() {
+                                                        execute("tests/integration-latest", "npm run dev:ci" + browser + ":react", function() {
                                                             //
                                                         });
                                                     });
