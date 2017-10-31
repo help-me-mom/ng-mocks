1523f5a6920dd2a9d9ec1ac58ac8a8825da48426
erikbarke <e@barke.nu>
2017-10-31 03:58:47 +0100

    Using gulp-typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var copy = require("copy"),
     clear = require("cli-clear"),
     del = require("del"),
-    exec = require("child_process").exec,
     gulp = require("gulp"),
     gutil = require("gulp-util"),
-    runSequence = require("run-sequence");
+    runSequence = require("run-sequence"),
+    sourcemaps = require("gulp-sourcemaps"),
+    ts = require("gulp-typescript"),
+    tsProject = ts.createProject("../../tsconfig.json");
 
 gulp.task("clear", function(cb) {
     clear();
@@ -13,19 +15,20 @@ gulp.task("clear", function(cb) {
 
 gulp.task("clean", function() {
     return del("./coverage")
-    .then(function(files) {
-        if(files.length > 0) {
-            gutil.log("Deleted %s", files);
-        }
-    });
+        .then(function(files) {
+            if(files.length > 0) {
+                gutil.log("Deleted %s", files);
+            }
+        });
 });
 
-gulp.task("build", function (cb) {
-    exec("../../node_modules/.bin/tsc --project ../../tsconfig.json --rootDir ../../src", function (error, stdout, stderr) {
-        process.stdout.write(stdout);
-        process.stderr.write(stderr);
-        cb(error);
-    });
+gulp.task("build", function() {
+    var tsResult = tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+    return tsResult.js
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(tsProject.options.outDir));
 });
 
 gulp.task("copy", function(cb) {
