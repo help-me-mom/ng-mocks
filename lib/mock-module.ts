49b2272c9b30e8728f04b2c4af1691d8548689b6
Isaac Datlof <ike18t@gmail.com>
2018-02-02 11:51:21 -0500

    feat: mock module providers

--- lib/mock-module.ts
+++ lib/mock-module.ts
@@ -9,16 +9,23 @@ const mockLookup: { [key: string]: any } = {
   Pipe: MockPipe
 };
 
-const mockIt = (declaration: any) => {
+const mockDeclaration = (declaration: any) => {
   const type = declaration.__annotations__[0].__proto__.ngMetadataName;
   return mockLookup[type](declaration);
 };
 
+const mockProvider = (provider: any) => ({
+  provide: provider, useValue: {}
+});
+
 export function MockModule<TModule>(module: Type<TModule>): Type<TModule> {
-  const mockedModule = { declarations: [] as any[] };
+  const mockedModule = { declarations: [] as any[],
+                         providers: [] as any[] };
   const declarations = (module as any).__annotations__[0].declarations || [];
   const imports = (module as any).__annotations__[0].imports || [];
+  const providers = (module as any).__annotations__[0].providers || [];
   mockedModule.declarations = [...imports.map(MockModule),
-                               ...declarations.map(mockIt)];
+                               ...declarations.map(mockDeclaration)];
+  mockedModule.providers = providers.map(mockProvider);
   return (mockedModule as any) as Type<TModule>;
 }
