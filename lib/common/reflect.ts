add374d7f79f479350f611cda189fd4f91996858
Brandon Domingue <brandon.domingue@adp.com>
2018-03-25 11:25:34 -0400

    feat: Support Angular propDecorators inputs and outputs

--- lib/common/reflect.ts
+++ lib/common/reflect.ts
@@ -13,5 +13,21 @@ export function getInputsOutputs(directive: Type<Component | Directive>, type: '
   return Object.keys(propertyMetadata)
                .filter((meta) => propertyMetadata[meta].find((m: any) => m.ngMetadataName === type))
                .reduce(metaReducer(propertyMetadata), [])
+               .concat(getPropDecorators(directive, type))
                .concat(getInputsOutputs((directive as any).__proto__, type));
+}
+
+function getPropDecorators(directive: any, type: 'Input' | 'Output') {
+  const decorators = directive.propDecorators;
+  if (!decorators) {
+    return [];
+  }
+  const fields: any[] = [];
+  Object.keys(decorators).forEach((propName) => {
+    (decorators[propName] as any[])
+      .filter((decorator: any) => decorator.type.prototype.ngMetadataName === type)
+      .forEach((decorator: any) =>
+        fields.push(`${propName}:${decorator.args[0] || propName}`));
+  });
+  return fields;
 }
