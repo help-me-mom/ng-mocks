89db09c0fd7367b381c8ea2a8cdb33e924c89d91
Isaac Datlof <ike18t@gmail.com>
2018-10-02 10:50:45 -0400

    test: fix angular bootstrap in tests

--- karma-test-shim.ts
+++ karma-test-shim.ts
@@ -1,5 +1,9 @@
 Error.stackTraceLimit = Infinity;
 
+// Reflect.metadata polyfill is only needed in the JIT mode which we use only for unit tests
+import 'core-js/es6/reflect'; // tslint:disable-line
+import 'core-js/es7/reflect'; // tslint:disable-line
+
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
