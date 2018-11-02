d5dca185449ed69a8326d30ec16d8abe7a600553
Michael Gusev <m@sudo.eu>
2018-11-02 15:00:09 +0100

    Support of @ViewChild

--- lib/mock-component/test-components/template-outlet.component.ts
+++ lib/mock-component/test-components/template-outlet.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, TemplateRef } from '@angular/core';
+import { Component, ContentChild, ElementRef, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'template-outlet-component',
@@ -6,9 +6,19 @@ import { Component, ContentChild, TemplateRef } from '@angular/core';
     <h1><template [ngTemplateOutlet]="injectedBlock1"></template></h1>
     <h2><template [ngTemplateOutlet]="injectedBlock2"></template></h2>
     <h3><ng-content></ng-content></h3>
+    <h4 #block4>own ref</h4>
   `,
 })
 export class TemplateOutletComponent {
+  // injected.
   @ContentChild('block1') injectedBlock1: TemplateRef<any>;
+
+  // injected.
   @ContentChild('block2') injectedBlock2: TemplateRef<any>;
+
+  // undefined (not injected).
+  @ContentChild('block3') injectedBlock3: TemplateRef<any>;
+
+  // ref to own template.
+  @ViewChild('block4') ownBlock3: ElementRef;
 }
