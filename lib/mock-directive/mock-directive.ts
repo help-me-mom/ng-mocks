52c5787e110eadce6cbf303b6bf33b6bee793f70
Michael Gusev <m@sudo.eu>
2018-12-24 13:25:37 +0100

    Way how to mock and test mocked Structural Directive

--- lib/mock-directive/mock-directive.ts
+++ lib/mock-directive/mock-directive.ts
@@ -1,10 +1,34 @@
 import {
-  Directive, EventEmitter, forwardRef, Inject, Optional, TemplateRef, Type, ViewContainerRef
+  Directive,
+  ElementRef,
+  EventEmitter,
+  forwardRef,
+  Optional,
+  TemplateRef,
+  Type,
+  ViewContainerRef
 } from '@angular/core';
 import { MockOf } from '../common';
 import { directiveResolver } from '../common/reflect';
 
 const cache = new Map<Type<Directive>, Type<Directive>>();
+
+export type MockDirective<T> = T & {
+  /** Pointer to current element in case of Attribute Directives. */
+  __element?: ElementRef;
+
+  /** Just a flag for easy understanding what it is. */
+  __isStructural: boolean;
+
+  /** Pointer to the template of Structural Directives. */
+  __template?: TemplateRef<any>;
+
+  /** Pointer to the view of Structural Directives. */
+  __viewContainer?: ViewContainerRef;
+
+  /** Helper function to render any Structural Directive with any context. */
+  __render($implicit?: any, variables?: {[key: string]: any}): void;
+};
 
 export function MockDirectives(...directives: Array<Type<any>>): Array<Type<any>> {
   return directives.map(MockDirective);
@@ -31,8 +55,16 @@ export function MockDirective<TDirective>(directive: Type<TDirective>): Type<TDi
     selector
   })
   class DirectiveMock {
-    constructor(@Optional() @Inject(TemplateRef) templateRef?: TemplateRef<any>,
-                @Optional() @Inject(ViewContainerRef) viewContainer?: ViewContainerRef) {
+
+    constructor(
+      @Optional() element?: ElementRef,
+      @Optional() template?: TemplateRef<any>,
+      @Optional() viewContainer?: ViewContainerRef,
+    ) {
+      (this as any).__element = element;
+      (this as any).__isStructural = viewContainer && template;
+      (this as any).__template = template;
+      (this as any).__viewContainer = viewContainer;
 
       Object.keys(directive.prototype).forEach((method) => {
         if (!(this as any)[method]) {
@@ -44,8 +76,19 @@ export function MockDirective<TDirective>(directive: Type<TDirective>): Type<TDi
         (this as any)[output.split(':')[0]] = new EventEmitter<any>();
       });
 
-      if (templateRef && viewContainer) {
-        viewContainer.createEmbeddedView(templateRef);
+      // Providing method to render mocked values.
+      (this as any).__render = ($implicit?: any, variables?: {[key: string]: any}) => {
+        if (viewContainer && template) {
+          viewContainer.clear();
+          viewContainer.createEmbeddedView(template, {...variables, $implicit});
+        }
+      };
+
+      // In case of structural directives we don't know how to render it properly when it's mocked.
+      // Because variables can be mapped, reduced, changed etc.
+      // Due to that we render it without any variables.
+      if (viewContainer && template) {
+        (this as any).__render();
       }
     }
   }
