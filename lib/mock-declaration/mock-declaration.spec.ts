99f166c931f055bde9edc635f6004cfce4b05d72
Michael Gusev <m@sudo.eu>
2020-02-20 23:08:00 +0100

    ISSUE #61: detection of an empty template

---
+++ lib/mock-declaration/mock-declaration.spec.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { MockDeclaration } from './mock-declaration';
+
+// tslint:disable:max-classes-per-file
+@Component({
+  selector: 'empty-template-container',
+  template: ''
+})
+export class EmptyTemplateContainer {
+}
+// tslint:enable:max-classes-per-file
+
+describe('MockDeclaration', () => {
+  it('should process components with an empty template correctly', () => {
+    const mockedComponent: any = MockDeclaration(EmptyTemplateContainer);
+    expect(mockedComponent.nameConstructor).toEqual('ComponentMock');
+  });
+});
