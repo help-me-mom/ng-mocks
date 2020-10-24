ac5fcf4d0d6b28db4610ef163c781193ca513039
MG <m@sudo.eu>
2020-10-24 09:09:56 +0200

    build: switching to semantic-release

---
+++ .lintstagedrc.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  '*': filenames => {
+    const commands = [];
+    const cwd = process.cwd();
+    const files = filenames
+      .filter(file => !file.match(/\.(ts)$/i))
+      .map(file => path.relative(cwd, file))
+      .map(file => `'${file}'`);
+    if (files.length === 0) {
+      return [];
+    }
+
+    commands.push(`prettier --write ${files.join(' ')}`);
+
+    return commands;
+  },
+  '*.ts': filenames => {
+    const commands = [];
+    const cwd = process.cwd();
+    const files = filenames.map(file => path.relative(cwd, file)).map(file => `'${file}'`);
+    if (files.length === 0) {
+      return [];
+    }
+
+    const filesForLint = filenames
+      .map(file => path.relative(cwd, file))
+      .filter(file => !file.match(/^e2e\//i))
+      .filter(file => !file.match(/^test\//i))
+      .map(file => `'${file}'`);
+
+    if (filesForLint.length) {
+      commands.push(`npm run lint -- --fix --force ${filesForLint.join(' ')}`);
+    }
+    commands.push(`prettier --write ${files.join(' ')}`);
+
+    return commands;
+  },
+};
---
+++ CONTRIBUTING.md
@@ -0,0 +1,36 @@
+# How to contribute to ngMocks
+
+The best way would be to discuss an issue or an improvement first:
+
+- [ask a question on gitter](https://gitter.im/ng-mocks/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+- [report it as an issue on github](https://github.com/ike18t/ng-mocks/issues)
+
+## How to release ngMocks
+
+It is possible on Unix based OS, Windows has not been supported.
+
+You need to create a `.env` file with the next content:
+
+```dotenv
+GH_TOKEN=<GITHUB_TOKEN>
+NPM_TOKEN=<NPM_TOKEN>
+GIT_AUTHOR_NAME=<YOUR_NAME>
+GIT_AUTHOR_EMAIL=<YOUR_EMAIL>
+GIT_COMMITTER_NAME=<YOUR_NAME>
+GIT_COMMITTER_EMAIL=<YOUR_EMAIL>
+```
+
+An example of it is:
+
+```dotenv
+GH_TOKEN=123123123
+NPM_TOKEN=123123123
+GIT_AUTHOR_NAME=Best Coder
+GIT_AUTHOR_EMAIL=best@coder.com
+GIT_COMMITTER_NAME=Best Coder
+GIT_COMMITTER_EMAIL=best@coder.com
+```
+
+After that you need to execute `npm run release`.
+
+Profit.
