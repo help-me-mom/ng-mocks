11859adcb43f51be73c3b685bf641a14ff07be76
MG <m@sudo.eu>
2020-11-23 22:16:58 +0100

    perf: cc cognitive complexity

--- lib/mock-service/helper.replace-with-mocks.ts
+++ lib/mock-service/helper.replace-with-mocks.ts
@@ -1,6 +1,70 @@
 import { NG_MOCKS_GUARDS } from '../common/core.tokens';
 import ngMocksUniverse from '../common/ng-mocks-universe';
 
+const handleSection = (section: any[]) => {
+  const guards: any[] = [];
+
+  for (const guard of section) {
+    if (ngMocksUniverse.builtDeclarations.has(guard) && ngMocksUniverse.builtDeclarations.get(guard) !== null) {
+      guards.push(guard);
+      continue;
+    }
+    if (
+      ngMocksUniverse.builtDeclarations.has(NG_MOCKS_GUARDS) &&
+      ngMocksUniverse.builtDeclarations.get(NG_MOCKS_GUARDS) === null
+    ) {
+      continue;
+    }
+    guards.push(guard);
+  }
+
+  return guards;
+};
+
+const handleArray = (value: any[], callback: any): [boolean, any[]] => {
+  const mock = [];
+  let updated = false;
+
+  for (const valueItem of value) {
+    if (ngMocksUniverse.builtDeclarations.has(valueItem) && ngMocksUniverse.builtDeclarations.get(valueItem) === null) {
+      updated = updated || true;
+      continue;
+    }
+    mock.push(callback(valueItem));
+    updated = updated || mock[mock.length - 1] !== valueItem;
+  }
+
+  return [updated, mock];
+};
+
+const handleItem = (value: Record<keyof any, any>, callback: any): [boolean, Record<keyof any, any>] => {
+  let mock: Record<keyof any, any> = {};
+  let updated = false;
+
+  for (const key of Object.keys(value)) {
+    if (
+      ngMocksUniverse.builtDeclarations.has(value[key]) &&
+      ngMocksUniverse.builtDeclarations.get(value[key]) === null
+    ) {
+      updated = updated || true;
+      continue;
+    }
+    mock[key] = callback(value[key]);
+    updated = updated || mock[key] !== value[key];
+  }
+
+  // Removal of guards.
+  for (const section of ['canActivate', 'canActivateChild', 'canDeactivate', 'canLoad']) {
+    const guards: any[] = Array.isArray(mock[section]) ? handleSection(mock[section]) : mock[section];
+    if (guards && mock[section].length !== guards.length) {
+      updated = updated || true;
+      mock = { ...mock, [section]: guards };
+    }
+  }
+
+  return [updated, mock];
+};
+
 const replaceWithMocks = (value: any): any => {
   if (ngMocksUniverse.cacheDeclarations.has(value)) {
     return ngMocksUniverse.cacheDeclarations.get(value);
@@ -13,60 +77,9 @@ const replaceWithMocks = (value: any): any => {
   let updated = false;
 
   if (Array.isArray(value)) {
-    mock = [];
-    for (const valueItem of value) {
-      if (
-        ngMocksUniverse.builtDeclarations.has(valueItem) &&
-        ngMocksUniverse.builtDeclarations.get(valueItem) === null
-      ) {
-        updated = updated || true;
-        continue;
-      }
-      mock.push(replaceWithMocks(valueItem));
-      updated = updated || mock[mock.length - 1] !== valueItem;
-    }
+    [updated, mock] = handleArray(value, replaceWithMocks);
   } else if (value) {
-    mock = {};
-    for (const key of Object.keys(value)) {
-      if (
-        ngMocksUniverse.builtDeclarations.has(value[key]) &&
-        ngMocksUniverse.builtDeclarations.get(value[key]) === null
-      ) {
-        updated = updated || true;
-        continue;
-      }
-      mock[key] = replaceWithMocks(value[key]);
-      updated = updated || mock[key] !== value[key];
-    }
-
-    // Removal of guards.
-    for (const section of ['canActivate', 'canActivateChild', 'canDeactivate', 'canLoad']) {
-      if (!Array.isArray(mock[section])) {
-        continue;
-      }
-
-      const guards: any[] = [];
-      for (const guard of mock[section]) {
-        if (ngMocksUniverse.builtDeclarations.has(guard) && ngMocksUniverse.builtDeclarations.get(guard) !== null) {
-          guards.push(guard);
-          continue;
-        }
-        if (
-          ngMocksUniverse.builtDeclarations.has(NG_MOCKS_GUARDS) &&
-          ngMocksUniverse.builtDeclarations.get(NG_MOCKS_GUARDS) === null
-        ) {
-          continue;
-        }
-        guards.push(guard);
-      }
-      if (mock[section].length !== guards.length) {
-        updated = updated || true;
-        mock = {
-          ...mock,
-          [section]: guards,
-        };
-      }
-    }
+    [updated, mock] = handleItem(value, replaceWithMocks);
   }
 
   if (updated) {
