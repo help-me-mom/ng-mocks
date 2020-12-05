0f838b50436ba46e99deef6ef60be154d6db3bbc
MG <m@sudo.eu>
2020-12-05 12:21:38 +0100

    build: run tests

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,35 +4,12 @@ module.exports = {
   '*': filenames => {
     const commands = [];
     const cwd = process.cwd();
-    const files = filenames
-      .filter(file => !file.match(/\.(ts)$/i))
-      .map(file => path.relative(cwd, file))
-      .map(file => `'${file}'`);
-    if (files.length === 0) {
-      return [];
-    }
-
-    commands.push(`prettier --write ${files.join(' ')}`);
-
-    return commands;
-  },
-  '*.ts': filenames => {
-    const commands = [];
-    const cwd = process.cwd();
     const files = filenames.map(file => path.relative(cwd, file)).map(file => `'${file}'`);
     if (files.length === 0) {
       return [];
     }
 
-    const filesForLint = filenames
-      .map(file => path.relative(cwd, file))
-      .filter(file => !file.match(/^e2e\//i))
-      .map(file => `'${file}'`);
-
     commands.push(`prettier --write ${files.join(' ')}`);
-    if (filesForLint.length) {
-      commands.push(`npm run lint`);
-    }
 
     return commands;
   },
