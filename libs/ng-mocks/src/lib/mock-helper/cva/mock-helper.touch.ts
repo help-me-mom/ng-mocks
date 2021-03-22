fd81409591d67c931f5686fd23210829179b8e75
MG <m@sudo.eu>
2021-03-22 22:05:58 +0100

    fix(#320): full implementation of ngMocks.touch and ngMocks.change

--- libs/ng-mocks/src/lib/mock-helper/cva/mock-helper.touch.ts
+++ libs/ng-mocks/src/lib/mock-helper/cva/mock-helper.touch.ts
@@ -1,27 +1,52 @@
-import { DebugNode } from '@angular/core';
+import { DebugElement } from '@angular/core';
 
+import coreForm from '../../common/core.form';
 import { isMockControlValueAccessor } from '../../common/func.is-mock-control-value-accessor';
 
 import funcGetVca from './func.get-vca';
 
-export default (el: DebugNode): void => {
-  const valueAccessor = funcGetVca(el);
+// default html behavior
+const triggerTouch = (el: DebugElement): void => {
+  const target = el.nativeElement;
+  el.triggerEventHandler('focus', {
+    target,
+  });
+
+  el.triggerEventHandler('blur', {
+    target,
+  });
+};
+
+const handleKnown = (valueAccessor: any): boolean => {
+  if (coreForm && valueAccessor instanceof coreForm.AbstractControl) {
+    valueAccessor.markAsTouched();
+
+    return true;
+  }
+
   if (isMockControlValueAccessor(valueAccessor.instance)) {
     valueAccessor.instance.__simulateTouch();
+
+    return true;
+  }
+
+  return false;
+};
+
+const keys = ['onTouched', '_onTouched', '_cvaOnTouch', '_markAsTouched', '_onTouchedCallback', 'onModelTouched'];
+
+export default (el: DebugElement): void => {
+  const valueAccessor = funcGetVca(el);
+  if (handleKnown(valueAccessor)) {
+    triggerTouch(el);
 
     return;
   }
 
-  for (const key of [
-    'onTouched',
-    '_onTouched',
-    '_cvaOnTouch',
-    '_markAsTouched',
-    '_onTouchedCallback',
-    'onModelTouched',
-  ]) {
+  for (const key of keys) {
     if (typeof valueAccessor[key] === 'function') {
-      valueAccessor[key]();
+      triggerTouch(el);
+      // valueAccessor[key]();
 
       return;
     }
