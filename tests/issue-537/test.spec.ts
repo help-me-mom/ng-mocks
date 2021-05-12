6b07067d751f29b4eba6d5db2773d605a35b3add
MG <m@sudo.eu>
2021-05-12 21:52:33 +0200

    test: covering ChangeDetectionStrategy.OnPush #537

---
+++ tests/issue-537/test.spec.ts
@@ -0,0 +1,45 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'target',
+  template: '{{ value }}',
+})
+class TargetComponent {
+  @Output() public readonly update = new EventEmitter<void>();
+  public value = 0;
+
+  public triggerUpdate(): void {
+    this.value += 1;
+    this.update.emit();
+  }
+}
+
+describe('issue-537', () => {
+  ngMocks.faster();
+  beforeAll(() => MockBuilder(TargetComponent));
+
+  it('renders default value w/o subscribers', () => {
+    const fixture = MockRender(TargetComponent);
+    expect(ngMocks.formatText(fixture)).toEqual('0');
+    fixture.point.componentInstance.triggerUpdate();
+    fixture.detectChanges();
+    expect(ngMocks.formatText(fixture)).toEqual('0');
+  });
+
+  it('renders changed value w/ subscribers', () => {
+    const fixture = MockRender(TargetComponent, {
+      update: undefined,
+    });
+    expect(ngMocks.formatText(fixture)).toEqual('0');
+    fixture.point.componentInstance.triggerUpdate();
+    fixture.detectChanges();
+    expect(ngMocks.formatText(fixture)).toEqual('1');
+  });
+});
