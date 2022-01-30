3d395bd4d8768cc5dfb1bffa53255966befc7d66
satanTime <m@sudo.eu>
2022-01-30 10:47:41 +0100

    test: restoring e2e

---
+++ tests-e2e/src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { MockBuilder, MockRender } from 'ng-mocks';
+
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+
+describe('AppComponent', () => {
+  beforeEach(() => MockBuilder(AppComponent, AppModule));
+
+  it('should create the app', () => {
+    const fixture = MockRender(AppComponent);
+    expect(fixture.point.componentInstance).toBeTruthy();
+  });
+
+  it(`should have a title`, () => {
+    const fixture = MockRender(AppComponent);
+    expect(fixture.point.componentInstance.title).toEqual(
+      'Hello World',
+    );
+  });
+
+  it('should render title', () => {
+    const fixture = MockRender(AppComponent);
+    expect(fixture.nativeElement.textContent).toContain(
+      fixture.point.componentInstance.title,
+    );
+  });
+});
