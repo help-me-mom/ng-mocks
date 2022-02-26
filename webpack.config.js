c3fd2be8480215033c62b0f14087ebf67cb4abb6
satanTime <m@sudo.eu>
2022-02-26 12:33:51 +0100

    build: support of mjs and module

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,75 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  entry: './libs/ng-mocks/src/index.ts',
-  target: ['web', 'es5'],
-  output: {
-    path: path.resolve(__dirname, './dist/libs/ng-mocks/'),
-    filename: 'index.js',
-    library: {
-      type: 'umd',
+module.exports = [
+  {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: './libs/ng-mocks/src/index.ts',
+    target: ['web', 'es5'],
+    output: {
+      path: path.resolve(__dirname, './dist/libs/ng-mocks/'),
+      filename: 'index.js',
+      library: {
+        type: 'umd',
+      },
+    },
+    externals: /^@angular\//,
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: path.resolve(__dirname, './libs/ng-mocks/tsconfig.json'),
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.cjs', '.mjs', '.ts', '.json'],
     },
   },
-  externals: /^@angular\//,
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: path.resolve(__dirname, './libs/ng-mocks/tsconfig.json'),
-              transpileOnly: true,
-            },
-          },
-        ],
+  {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: './libs/ng-mocks/src/index.ts',
+    target: ['web', 'es5'],
+    experiments: {
+      outputModule: true,
+    },
+    output: {
+      path: path.resolve(__dirname, './dist/libs/ng-mocks/'),
+      filename: 'index.mjs',
+      library: {
+        type: 'module',
       },
-    ],
+      environment: { module: true },
+    },
+    externals: /^@angular\//,
+    externalsType: 'module',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: path.resolve(__dirname, './libs/ng-mocks/tsconfig.esm.json'),
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.cjs', '.mjs', '.ts', '.json'],
+    },
   },
-  resolve: {
-    extensions: ['.js', '.cjs', '.mjs', '.ts', '.json'],
-  },
-};
+];
