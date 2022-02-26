5fce75f3745185fb650cc2930505a92993987b64
satanTime <m@sudo.eu>
2022-02-26 20:12:28 +0100

    docs: example of structural directives with let and of #1918

---
+++ examples/TestDirectiveLetOf/static.spec.ts
@@ -0,0 +1,51 @@
+import {
+  Directive,
+  Input,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
+
+@Directive({
+  selector: '[dxTemplate]',
+})
+class DxTemplateDirective {
+  @Input() public readonly dxTemplateOf: string | null = null;
+
+  public constructor(
+    protected templateRef: TemplateRef<any>,
+    protected viewContainerRef: ViewContainerRef,
+  ) {}
+}
+
+describe('TestDirectiveLetOf:static', () => {
+  beforeEach(() =>
+    MockBuilder().mock(DxTemplateDirective, {
+      // We should not only render the structural directive,
+      // but also provide its context variables.
+      render: {
+        $implicit: {
+          data: 'MOCK_DATA',
+        },
+      },
+    }),
+  );
+
+  it('renders a mock of structural directives', () => {
+    const fixture = MockRender(`
+      <div *dxTemplate="let cellTemplate of 'actionsCellTemplate'">
+        {{ cellTemplate.data }}
+      </div>
+    `);
+
+    // firstly, let's check that we passed 'actionsCellTemplate' as input value.
+    expect(
+      ngMocks.findInstance(DxTemplateDirective).dxTemplateOf,
+    ).toEqual('actionsCellTemplate');
+
+    // secondly, let's check that MOCK_DATA has been rendered.
+    expect(ngMocks.formatHtml(fixture)).toEqual(
+      '<div> MOCK_DATA </div>',
+    );
+  });
+});
