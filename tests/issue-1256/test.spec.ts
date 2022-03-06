a903556c7aa5c101cf44a3c1f6d20a92e5c74698
satanTime <m@sudo.eu>
2022-03-06 19:59:32 +0100

    feat(MockInstance): resets root overrides likewise properties #1256

---
+++ tests/issue-1256/test.spec.ts
@@ -0,0 +1,28 @@
+import { InjectionToken } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { MockInstance, MockProvider, MockReset } from 'ng-mocks';
+
+const myToken = new InjectionToken('MY_TOKEN');
+
+// @see https://github.com/ike18t/ng-mocks/issues/1256
+// global MockInstance doesn't reset own customizations
+describe('issue-1256', () => {
+  MockInstance.scope();
+
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [MockProvider(myToken, { test: 1 })],
+    }).compileComponents(),
+  );
+
+  it('changes value #1', () => {
+    MockInstance(myToken, () => ({ test: 2 }));
+    const value: any = TestBed.get(myToken);
+    expect(value.test).toEqual(2);
+  });
+
+  it('uses the default value', () => {
+    const value: any = TestBed.get(myToken);
+    expect(value.test).toEqual(1);
+  });
+});
