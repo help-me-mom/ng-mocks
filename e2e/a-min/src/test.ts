4d4a088337369dd62cfb519d5a3f9852f784e2ec
satanTime <m@sudo.eu>
2022-04-07 18:56:20 +0200

    test: no zone.js/testing #2195

--- e2e/a-min/src/test.ts
+++ e2e/a-min/src/test.ts
@@ -1,7 +1,5 @@
 // tslint:disable no-import-side-effect ordered-imports
 
-import 'zone.js/dist/zone-testing';
-
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
