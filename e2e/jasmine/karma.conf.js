6abfa6a46304b0d854b534e51486aca216aa016b
satanTime <m@sudo.eu>
2025-12-20 18:55:38 +0100

    chore(e2e/jasmine): sync (#12671)

--- e2e/jasmine/karma.conf.js
+++ e2e/jasmine/karma.conf.js
@@ -1,18 +1,13 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/6.4/config/configuration-file.html
 
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
+    frameworks: ['jasmine'],
+    plugins: [require('karma-jasmine'), require('karma-chrome-launcher'), require('karma-jasmine-html-reporter')],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
       jasmine: {
@@ -29,8 +24,6 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
     browsers: ['ChromeCi'],
-    singleRun: true,
   });
 };
