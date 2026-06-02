dd3ae76e31229cbb5b81052cae26b3469d976c90
satanTime <m@sudo.eu>
2026-06-03 00:05:38 +0200

    fix(build): compact modern declaration entrypoint #14003

--- libs/ng-mocks/src/index.ts
+++ libs/ng-mocks/src/index.ts
@@ -1,3 +1,13 @@
 // istanbul ignore file
 
-export * from './index.modern';
+export * from './index.common';
+export { MockRender } from './lib/mock-render/mock-render';
+export { MockRenderFactory } from './lib/mock-render/mock-render-factory';
+export type {
+  DefaultRenderComponent,
+  IMockRenderFactoryOptions,
+  IMockRenderOptions,
+  MockedComponentFixture,
+  MockedDebugElement,
+  MockedDebugNode,
+} from './lib/mock-render/types';
