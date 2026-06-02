06657187413c27613d361f75757cb45493c10217
satanTime <m@sudo.eu>
2026-06-02 23:49:14 +0200

    fix(build): align modern declaration entrypoint #14003

--- libs/ng-mocks/src/index.ts
+++ libs/ng-mocks/src/index.ts
@@ -1,13 +1,3 @@
 // istanbul ignore file
 
-export * from './index.common';
-export { MockRender } from './lib/mock-render/mock-render';
-export { MockRenderFactory } from './lib/mock-render/mock-render-factory';
-export type {
-  DefaultRenderComponent,
-  IMockRenderFactoryOptions,
-  IMockRenderOptions,
-  MockedComponentFixture,
-  MockedDebugElement,
-  MockedDebugNode,
-} from './lib/mock-render/types';
+export * from './index.modern';
