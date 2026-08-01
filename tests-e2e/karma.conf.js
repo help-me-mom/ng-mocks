6d2f7f14ff267855c49029de327fa95372941eb7
satanTime <m@sudo.eu>
2026-08-01 18:47:32 +0200

    chore(tests-e2e): resolve Angular 22 updates

--- tests-e2e/karma.conf.js
+++ tests-e2e/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/6.4/config/configuration-file.html
 
 const { executablePath } = require('puppeteer');
 
@@ -8,13 +8,8 @@ module.exports = async function (config) {
 
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
+    frameworks: ['jasmine'],
+    plugins: [require('karma-jasmine'), require('karma-chrome-launcher'), require('karma-jasmine-html-reporter')],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
       jasmine: {
--- tests-e2e/src/issue-4693/test.spec.ts
+++ tests-e2e/src/issue-4693/test.spec.ts
@@ -9,11 +9,18 @@ import { ChildComponent } from './child.component';
   template: '',
 })
 class TargetComponent implements OnInit {
+  public readonly childLoaded = new Promise<void>(resolve => {
+    this.resolveChildLoaded = resolve;
+  });
+
+  private resolveChildLoaded!: () => void;
+
   constructor(public readonly containerRef: ViewContainerRef) {}
 
   async ngOnInit() {
     const { ChildComponent } = await import('./child.component');
     this.containerRef.createComponent(ChildComponent);
+    this.resolveChildLoaded();
   }
   public targetComponent4693() {}
 }
@@ -24,7 +31,9 @@ describe('issue-4693', () => {
 
     it('loads lazy component', async () => {
       const fixture = MockRender(TargetComponent);
-      await fixture.whenStable();
+      fixture.detectChanges();
+      await fixture.componentInstance.childLoaded;
+      fixture.detectChanges();
       // eslint-disable-next-line es-x/no-array-prototype-find -- ngMocks.find is not Array.find.
       const el = ngMocks.find(ChildComponent);
       expect(ngMocks.formatText(el)).toEqual('child');
@@ -39,7 +48,9 @@ describe('issue-4693', () => {
 
     it('loads lazy component as a mock', async () => {
       const fixture = MockRender(TargetComponent);
-      await fixture.whenStable();
+      fixture.detectChanges();
+      await fixture.componentInstance.childLoaded;
+      fixture.detectChanges();
       // eslint-disable-next-line es-x/no-array-prototype-find -- ngMocks.find is not Array.find.
       const el = ngMocks.find(ChildComponent);
       expect(ngMocks.formatText(el)).toEqual('');
